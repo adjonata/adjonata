8d0def0283f29688570a7ae912887cc1fe5aad2e
alexdjonata <alexdjonata30@gmail.com>
2020-12-30 15:26:59 -0300

    Tornando restritas as rotas de alteração dos dados

--- portfolio/node-api/src/routes/social.routes.ts
+++ portfolio/node-api/src/routes/social.routes.ts
@@ -3,22 +3,31 @@ import { Router } from "express";
 import SocialController from "../controllers/social.controller";
 import SocialValidation from "../validation/social.valid";
 import { verifyRequest } from "../utils/validation";
+import { verifyJWT } from "../middlewares/auth";
 
 const SocialRouter = Router();
 
 SocialRouter.get("/", SocialController.getter);
 SocialRouter.post(
   "/",
+  verifyJWT,
   SocialValidation.create,
   verifyRequest,
   SocialController.create
 );
 SocialRouter.put(
   "/:id",
+  verifyJWT,
   SocialValidation.edit,
   verifyRequest,
   SocialController.edit
 );
-SocialRouter.delete("/:id", SocialController.delete);
+SocialRouter.delete(
+  "/:id",
+  verifyJWT,
+  SocialValidation.delete,
+  verifyRequest,
+  SocialController.delete
+);
 
 export default SocialRouter;
